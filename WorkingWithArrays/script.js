43eee0587b19f6949a5eb5c51ea5de203451d6c9
Jidraph1 <jidraphkimachia1@gmail.com>
2024-04-17 10:59:13 +0300

    refactor

--- WorkingWithArrays/script.js
+++ WorkingWithArrays/script.js
@@ -74,3 +74,6 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+
+console.log(movements.slice(2,6))
